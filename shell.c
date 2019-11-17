f2a273c3d044d2f3de8a2653d505749b51907b2e
nickuchida <nicholas.y.uchida@gmail.com>
2019-11-17 20:47:50 +0000

    adding prompt and tokenizing to shell

--- shell.c
+++ shell.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
 
 int main (void)
 {
+	char *buffer = NULL;
+	char *token;
+	size_t bufflen = 0;
+
+	while (1)
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		getline(&buffer, &bufflen, stdin);
+
+		token = strtok(buffer, " ");
+		while (token != NULL)
+		{
+			write(STDOUT_FILENO, token, _strlen(token));
+			token = strtok(NULL, " ");
+		}
+		free(token);
+	}
+	return (0);
 }
