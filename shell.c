b8313bfab513c154fb578fe25e8a9bfbd9e97b79
ACholberton <1103@holbertonschool.com>
2019-11-18 02:19:27 +0000

    added new variable and helper functions

--- shell.c
+++ shell.c
@@ -1,5 +1,56 @@
 #include "holberton.h"
+/**
+ *_strdup - will return a pointer to a newly allocated space in memory
+ *@str: is a string
+ *Return: a pointer
+ */
+char *_strdup(char *str)
+{
+	char *point;
 
+	if (str == '\0')
+	{
+		return ('\0');
+	}
+
+	point = malloc(_strlen(str) + 1);
+
+	if (point == '\0')
+	{
+		return ('\0');
+	}
+
+	_strcpy(point, str);
+
+	return (point);
+}
+/**
+ **_strcpy - will copy a string to a buffer;
+ *@dest: is a buffer
+ *@src: is a string
+ *Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a, c;
+
+	a = 0;
+
+	while (src[a] != '\0')
+	{
+		a++;
+	}
+	for (c = 0; c <= a; c++)
+	{
+		dest[c] = src[c];
+	}
+	return (dest);
+}
+/**
+ *_strlen - this function returns the lenght of  string
+ *@s: is a string
+ *Return: lenght
+ */
 int _strlen(char *s)
 {
 	int i = 0;
@@ -15,14 +66,21 @@ int main (int ac, char **av, char **env)
 	char *token;
 	size_t bufflen = 0;
 	char **str;
-	size_t counter = 0;
+	size_t counter = 0, i = 0;
+	char *buffer_cpy = NULL;
 
 	while (1)
 	{
 		write(STDOUT_FILENO, "$ ", 2);
-		getline(&buffer, &bufflen, stdin);
+		getline(&buffer, &bufflen, stdin); //prompt//
 
-		token = strtok(buffer, " ");
+		for (; buffer[i] != '\0'; i++)
+			if (buffer[i] == '\n')
+			{
+				buffer[i] = '\0';
+			}
+		buffer_cpy = _strdup(buffer);
+		token = strtok(buffer_cpy, " ");
 		while(token != NULL)
 		{
 			token = strtok(NULL, " ");
@@ -34,19 +92,14 @@ int main (int ac, char **av, char **env)
 		{
 			str[counter] = token;
 			write(STDOUT_FILENO, token, _strlen(token));
-			if (fork() == 0)
-				execve(str[0], str, NULL);
-			else
-				wait(NULL);
 			token = strtok(NULL, " ");
 			counter++;
 		}
-/**
+		str[i] = NULL;
 		if (fork() == 0)
 			execve(str[0], str,  NULL);
 		else
 			wait(NULL);
-*/
 		free(token);
 	}
 	return (0);
