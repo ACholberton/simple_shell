e0fb054064cd15b077b2b1cfe17fda5ad7b7b992
ACholberton <1103@holbertonschool.com>
2019-11-19 21:42:50 +0000

    added strcmp prototype

--- helperfunctions.c
+++ helperfunctions.c
@@ -1,5 +1,29 @@
 #include "holberton.h"
+/**
+ *_strcmp - will compare two strings
+ *@s1: is a string
+ *@s2: is a string
+ *Return: a string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a, compare;
 
+	for (a = 0; s1[a] != '\0';)
+	{
+		if (s1[a] == s2[a])
+		{
+			compare = s1[a] - s2[a];
+			a++;
+		}
+		else
+		{
+			compare = s1[a] - s2[a];
+			break;
+		}
+	}
+	return (compare);
+}
 /**
  *_strdup - will return a pointer to a newly allocated space in memory
  *@str: is a string
