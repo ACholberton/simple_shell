87805ebdd8448d86f00c4c0524fb85b20acf5fcf
ACholberton <1103@holbertonschool.com>
2019-11-20 21:59:35 +0000

    added env and exit buit-in

--- shell.c
+++ shell.c
@@ -17,16 +17,23 @@ int main(void)
 			perror("Invalid task, try again");
 			exit(1);
 		}
-
 		write(STDOUT_FILENO, "$ ", 2);
 		getline(&buffer, &bufflen, stdin);
-
 		for (; buffer[i] != '\0'; i++)
 		{
 			if (buffer[i] == '\n')
 				buffer[i] = '\0';
 		}
-
+		if (strcmp(buffer, "env") == 0)
+		{
+			printenviron();
+		}
+		if (strcmp(buffer, "exit") == 0)
+		{
+			write(STDOUT_FILENO, "cya", 4);
+			_putchar('\n');
+			exit(0);
+		}
 		command = tokens(buffer);
 
 		if (fork() == 0)
