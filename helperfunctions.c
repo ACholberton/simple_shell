263bfa2b32712a740860a33f3c41d1569d083d43
ACholberton <1103@holbertonschool.com>
2019-11-20 00:16:22 +0000

    added _putchar

--- helperfunctions.c
+++ helperfunctions.c
@@ -84,3 +84,14 @@ int _strlen(char *s)
 		i++;
 	return (i);
 }
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
