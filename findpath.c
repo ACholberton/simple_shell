c41402ef6fd7eab3863e8e31c41d4905494acd1f
nickuchida <nicholas.y.uchida@gmail.com>
2019-11-21 21:50:14 +0000

    updating findpath, malloc, using access to check path, executing commands

--- findpath.c
+++ findpath.c
@@ -2,27 +2,37 @@
 
 void *findpath(char *input, char **env)
 {
-	char *left, *right, *bin;
+	char *left, *right, *bins = NULL, *token, *path = NULL;
 	size_t i = 0;
 
 
-	while (environ[i])
+	left = strtok(*env, "=");
+	right = strtok(NULL, "=");
+	if (_strcmp(left, "PATH") == 0)
 	{
-		env_copy = _strdup(environ[i]);
-		left = strtok(env_copy, "=");
-		right = strtok(NULL, "=");
-		if (strcmp(left, "PATH") == 0)
+		bins = right;
+		token = strtok(bins, ":");
+		while (bins)
 		{
+			token = strtok(NULL, ":");
 
-			bins = right
-			while (bins)
+			path = malloc(sizeof(char *) * _strlen(token));
+
+			path = _strcat(bins, "/");
+			path = _strcat(path, input);
+			path = _strcat(path, "\0");
+
+			if (access(path, F_OK) == 0)
 			{
-				strtok(NULL, ":");
+				if (fork() == 0)
+					execve(path[0], path, NULL);
+				else
+					wait(NULL);
 			}
 		}
+		path = NULL;
 		i++;
 		free(left);
 		free(right);
 	}
-	return (dup_copy);
 }
