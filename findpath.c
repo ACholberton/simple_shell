d78950ca62e0fe42c5869e98495c65730ecd3228
nickuchida <nicholas.y.uchida@gmail.com>
2019-11-22 02:14:57 +0000

    fixing the execute and forking to finally print ls!

--- findpath.c
+++ findpath.c
@@ -4,17 +4,17 @@ int findpath(char **command, char **env)
 {
 	char *left = NULL, *right = NULL, *bins, *token, *envdup, *path = NULL;
 	size_t i, j, len1, len2;
+	int a;
 
 	for (i = 0; env[i]; i++)
 	{
-		envdup = _strdup(*env);
+		envdup = _strdup(env[i]);
 		left = strtok(envdup, "=");
 
 		right = strtok(NULL, "=");
 
 		if (_strcmp(left, "PATH") == 0)
 		{
-			printf("test");
 			bins = right;
 			token = strtok(bins, ":");
 			for (j = 0; bins; j++)
@@ -25,15 +25,23 @@ int findpath(char **command, char **env)
 
 				path = malloc(sizeof(char *) * (len1 + len2));
 
-				_strcat(path, bins);
+				_strcat(path, token);
 				_strcat(path, "/");
 				_strcat(path, *command);
 				_strcat(path, "\0");
 
+				a = fork();
 				if (access(path, X_OK) == 0)
-					execve(command[0], command, NULL);
-				else
-					wait(NULL);
+				{
+					if (a == 0)
+					{
+						execve(path, command, NULL);
+						break;
+					}
+					else
+						wait(NULL);
+				}
+
 			}
 		}
 		env++;
