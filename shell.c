8cfd44bc57963739e7187280e9e8cdf28cc4236a
ACholberton <1103@holbertonschool.com>
2019-11-23 00:45:28 +0000

    made changes for memory alloc

--- shell.c
+++ shell.c
@@ -1,6 +1,9 @@
 #include "holberton.h"
 /**
  *main - is the entry point
+ *@ac:  is the number of arguments passed
+ *@av: is the argument passed
+ *@env: is the enviroment
  *Return: always 0
  */
 int main(int ac, char **av, char **env)
@@ -17,16 +20,18 @@ int main(int ac, char **av, char **env)
 		i = 0;
 		write(STDOUT_FILENO, "$ ", 2);
 		getline(&buffer, &bufflen, stdin);
+		if(_strcmp(buffer, "\n") == 0)
+			continue;
 		for (; buffer[i] != '\0'; i++)
 		{
 			if (buffer[i] == '\n')
 				buffer[i] = '\0';
 		}
-		if (strcmp(buffer, "env") == 0)
+		if (_strcmp(buffer, "env") == 0)
 		{
-			printenv(env);
+			printenvi(env);
 		}
-		if (strcmp(buffer, "exit") == 0)
+		if (_strcmp(buffer, "exit") == 0)
 		{
 			write(STDOUT_FILENO, "cya", 4);
 			_putchar('\n');
@@ -35,16 +40,14 @@ int main(int ac, char **av, char **env)
 		}
 		command = tokens(buffer);
 		new = findpath(command, env);
-/*		if (buffer == new)
-		{
-			perror(*command);
-			}*/
 		if (fork() == 0)
 			execve(new, command,  NULL);
 		else
 			wait(NULL);
 
 		free(buffer);
+		if (new == NULL)
+			free(new);
 		buffer = NULL;
 	}
 	return (0);
