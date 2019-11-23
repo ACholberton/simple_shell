c19db6084cdfb32fbf4eeaab244554f5094a6a47
ACholberton <1103@holbertonschool.com>
2019-11-23 00:47:47 +0000

    fixing memory alloc problems

--- findpath.c
+++ findpath.c
@@ -4,7 +4,6 @@ char *findpath(char **command, char **env)
 {
 	char *left = NULL, *right = NULL, *bins, *token, *envdup, *path = NULL;
 	size_t i, j, len1 = 0, len2 = 0;
-	/*int a;*/
 
 	for (i = 0; env[i]; i++)
 	{
@@ -23,28 +22,25 @@ char *findpath(char **command, char **env)
 				len1 = _strlen(token);
 				len2 = _strlen(command[0]);
 
-				path = malloc(sizeof(char *) * (len1 + len2));
-
+				path = malloc(sizeof(char) * (len1 + len2));
 				_strcat(path, token);
 				_strcat(path, "/");
 				_strcat(path, *command);
 				_strcat(path, "\0");
 
-				/*a = fork();*/
 				if (access(path, X_OK) == 0)
 				{
 					return (path);
-					/*	if (a == 0)
-					{
-						execve(path, command, NULL);
-					}
-					else
-					wait(NULL);*/
+				}
+				else
+				{
+					free(path);
 				}
 			}
 		}
 		env++;
-		free(path);
+		free(envdup);
+		envdup = NULL;
 		path = NULL;
 	}
 	return (*command);
